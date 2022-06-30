22a03e3d430aa790d0bc96a7f68987cf8901f764
yohanaFalconi <yohanafalconivivas@gmail.com>
2022-06-30 08:27:10 -0500

    sigIn and Navigate css screen done

--- src/Components/Navigate/Navigate.js
+++ src/Components/Navigate/Navigate.js
@@ -3,19 +3,21 @@ import { Link } from "react-router-dom";
 
 function Navigate() {
     return(
-        <div className="burgerBg container">
-            <button>
-                <Link to='/waiter-view'>
-                    <p>I'm a waiter or waitress</p>
-                    <p>Place orders</p>
-                </Link>
-            </button>
-            <button>
-                <Link to='/chef-view'>
-                    <p>I'm a chef</p>
-                    <p>See orders</p>
-                </Link>
-            </button>
+        <div className="burgerBg">
+            <div className='flex justify-center gap-16'>
+                <button className='navBtn'>
+                    <Link to='/waiter-view'>
+                        <p className='text-2xl'>I'm a waiter or waitress</p>
+                        <p>Place orders</p>
+                    </Link>
+                </button>
+                <button className='navBtn'>
+                    <Link to='/chef-view'>
+                        <p className='text-2xl'>I'm a chef</p>
+                        <p>See orders</p>
+                    </Link>
+                </button>   
+            </div>
         </div>
     );
 }
