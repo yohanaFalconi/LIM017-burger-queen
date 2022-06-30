52da94b43767ef70decaedd1a8ac1129a961f890
Megan Palacios <meg2086@outlook.com>
2022-06-30 10:42:21 -0500

    Styling WaiterView: installed poppins font-family with tailwind, added colors of the prototype palette, gave basic style to product item cards

--- src/Components/WaiterView/WaiterView.js
+++ src/Components/WaiterView/WaiterView.js
@@ -8,9 +8,9 @@ import { useEffect, useState } from 'react';
 const waiter = 'Megan';
 function WaiterView() {
     return (
-        <div className='bg-slate-50 WaiterView'>
+        <div className='bg-[#FAFAFA] WaiterView'>
             <main className='main'>
-                <img src={bqlogo} alt='Burger Queen' className='h-32' />
+                <img src={bqlogo} alt='Burger Queen' className='h-24 ml-4 mt-3' />
                 <Products />
             </main>
             <aside className='aside'>
@@ -22,11 +22,11 @@ function WaiterView() {
 
 function ProductItem(props) {
     return(
-        <li key={props.item.id} className='bg-white shadow-md rounded-2xl m-3'>
+        <li key={props.item.id} className='bg-white shadow-md rounded-2xl text-center h-4/5 font-poppins'>
+            <img src={props.item.data.url} alt={props.item.data.Name} className='h-1/2 m-3 max-w-[80%] inline-grid' />
             <h4>{props.item.data.Name}</h4>
-            <img src={props.item.data.url} alt={props.item.data.Name} />
-            <p>Price: {props.item.data.Price}</p>
-            <div>
+            <p>${props.item.data.Price}</p>
+            <div className='flex justify-center'>
                 <button>-</button>
                 <p>{props.item.data.Count}</p>
                 <button>+</button>
@@ -60,7 +60,7 @@ function Products() {
         return <div>Loading menu...</div>;
     }
     return (
-        <div className='menu grid grid-cols-4'>
+        <div className='menu grid grid-cols-4 gap-5 m-5'>
             <ul className='contents'>
                 {items.map(item => <ProductItem item={item} />)}
             </ul>
@@ -70,11 +70,11 @@ function Products() {
 
 function Order() {
     return(
-        <div>
-            <div>Waiter: {waiter}</div>
+        <div className='bg-[#B5D6B2] rounded-2xl h-screen mt-5 font-poppins'>
+            <div className='bg-[#FAFAFA]'>Waiter: {waiter}</div>
             <label>
                 Table:
-                <select name="select">
+                <select name="select" className='bg-[#FAFAFA]'>
                     <option value="table1">1</option>
                     <option value="table2">2</option>
                     <option value="table3">3</option>
@@ -86,7 +86,7 @@ function Order() {
             <div>Ordered items go here</div>
             <Icon color="blue" size={20} icon="plus" />
             <hr />
-            <div>Total: $0</div>
+            <div className='bg-[#FFBF69]'>Total: $0</div>
             <div>
                 <img src='#' alt='delete'></img>
                 <button>Send order</button>
