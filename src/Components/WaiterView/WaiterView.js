cca8b969eef85f3fcc5eb98fdf9fec623c6194da
Megan Palacios <meg2086@outlook.com>
2022-07-10 12:05:06 -0500

    BROKEN! trying to change styles on nav buttons depending on which component is being rendered, conditionals not working, skipping to default/else case

--- src/Components/WaiterView/WaiterView.js
+++ src/Components/WaiterView/WaiterView.js
@@ -2,6 +2,8 @@ import './WaiterView.css';
 import bqlogo from '../../assets/bqlogo.png';
 import { getItemsById } from '../../firebase-utils';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const addProductQty = (props,id) => {
     console.log('props',props,'id',id);
@@ -42,28 +44,69 @@ const subtracProductQty = (props,id) => {
 }
 
 function WaiterView() {
+    
+    const [placeOrdersState, setPlaceOrdersState] = useState('inactive');
+    const [readyServeState, setReadyServeState] = useState('inactive');
+
+    let location = useLocation().pathname;
+    console.log(useLocation());
+
+    useEffect(() => {
+        console.log('location has been changed to:', location);
+        switch (location) {
+            case 'waiter-view/place-orders':
+                console.log('help');
+                setPlaceOrdersState('active');
+                setReadyServeState('inactive');
+                break;
+            case 'waiter-view/ready-to-serve':
+                console.log('ayuda');
+                setPlaceOrdersState('inactive');
+                setReadyServeState('active');
+                break;
+            default:
+                console.log('something');
+                break;
+        }
+        /* if (location === 'waiter-view/place-orders') {
+            console.log('help');
+            setPlaceOrdersState('active');
+            setReadyServeState('inactive');
+        } else if (location === 'waiter-view/ready-to-serve') {
+            console.log('ayuda');
+            setPlaceOrdersState('inactive');
+            setReadyServeState('active');
+        } else {
+            console.log('something');
+        } */
+    }, [location])
+
+    // cada botón va a tener su estado, que va a ser llamado en un data attribute
+    // y estilado en el css
+    // change the state on useEffect
+
     return (
         <header className='grid grid-flow-col fixed top-0 w-[100vw] bg-[#FAFAFA]'>
             <Link to='/navigate'>
                 <img src={bqlogo} alt='Burger Queen' className='h-[13vh] p-2 ml-3' />
             </Link>
             <nav className='self-center mr-4 flex justify-evenly'>
-                <button className='font-medium bg-[#FFBF69] text-black shadow-md rounded-2xl px-[6%] py-[1%]'>
+                <button className={placeOrdersState}>
                     <Link to='/waiter-view/place-orders'>
                         Place orders
                     </Link>
                 </button>
-                <button className='font-medium bg-[#FFBF69] text-black shadow-md rounded-2xl px-[6%] py-[1%] mx-3'>
+                <button className={readyServeState}>
                     <Link to='/waiter-view/ready-to-serve'>
                         See ready to serve
                     </Link>
                 </button>
-                <button className='font-medium bg-[#1B1A1A] text-white shadow-md rounded-2xl px-[6%] py-[1%]'>
+                <button className='font-medium bg-[#1B1A1A] hover:bg-[#FE9C08] text-white shadow-md rounded-2xl px-[6%] py-[1%]'>
                     Log out
                 </button>
             </nav>
         </header>
     );
-}
+};
 
 export { WaiterView, addProductQty, subtracProductQty };
