3792f0606fa74ba6e84d2f5aa7ccf73918189809
Megan Palacios <meg2086@outlook.com>
2022-07-12 00:49:30 -0500

    made order invoice selected products scrollable

--- src/Components/OrderInvoice/OrderInvoice.js
+++ src/Components/OrderInvoice/OrderInvoice.js
@@ -3,7 +3,7 @@ import CartItems from "../CartItems/CartItems";
 
 export default function OrderInvoice(props) {
     return(
-        <div className='bg-[#B5D6B2] shadow-md rounded-2xl h-[80vh] mt-5 font-poppins font-normal flex flex-col fixed'>
+        <div className='bg-[#B5D6B2] shadow-md rounded-2xl h-[80vh] w-[28vw] mt-5 font-poppins font-normal flex flex-col fixed'>
             <div className='bg-[#FAFAFA] shadow-md rounded-2xl my-[2vh] mx-[1vw] px-[6%] py-[1%]'>
                 Waiter: {props.username}
             </div>
@@ -18,22 +18,28 @@ export default function OrderInvoice(props) {
                 </select>
             </div>
             <hr className='w-[90%] mx-[5%]' />
-            <CartItems 
-                key={props.id}
-                selected={props.selected} 
-                setSelected={props.setSelected} 
-            />
-            <hr className='w-[90%] mx-[5%]' />
-            <div className='bg-[#FFBF69] shadow-md rounded-2xl my-[2vh] mx-[1vw] px-[6%] py-[1%] grid grid-flow-col justify-between'>
-                <p>Total:</p>
-                <p>${props.total}</p>
+            <div className="max-h-[46vh] overflow-auto">
+                <CartItems 
+                    key={props.id}
+                    selected={props.selected} 
+                    setSelected={props.setSelected} 
+                />
             </div>
-            <div>
-                <Icon color="#1B1A1A" size={26} icon="bin" className='mx-[1.5vw]' />
-                <button className='font-medium bg-[#1B1A1A] hover:bg-[#FE9C08] text-white shadow-md rounded-2xl px-[6%] py-[1%] w-[20vw] mr-3'>
-                    Send order
-                </button>
+            
+            <div className="fixed bottom-0 mb-[35px]">
+                <hr className='w-[90%] mx-[5%]' />
+                <div className='bg-[#FFBF69] shadow-md rounded-2xl my-[2vh] mx-[1vw] px-[6%] py-[1%] grid grid-flow-col justify-between'>
+                    <p>Total:</p>
+                    <p>${props.total}</p>
+                </div>
+                <div>
+                    <Icon color="#1B1A1A" size={26} icon="bin" className='mx-[1.5vw]' />
+                    <button className='font-medium bg-[#1B1A1A] hover:bg-[#FE9C08] text-white shadow-md rounded-2xl px-[6%] py-[1%] w-[20vw] mr-3'>
+                        Send order
+                    </button>
+                </div>
             </div>
+            
         </div>
     );
 }
