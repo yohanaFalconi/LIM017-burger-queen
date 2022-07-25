726312b62a402cd4c322d50793ea50acf176e351
Megan Palacios <meg2086@outlook.com>
2022-07-25 10:39:55 -0500

    final styles for chef view

--- src/Components/ChefView/ChefView.js
+++ src/Components/ChefView/ChefView.js
@@ -1,4 +1,5 @@
-import './ChefView.css'
+import './ChefView.css';
+import Icon from "../../IcoMoon/Icon";
 import bqlogo from '../../assets/bqlogo.png';
 import FullOrders from '../FullOrders/FullOrders';
 import { useEffect, useState } from 'react';
@@ -37,18 +38,24 @@ export default function ChefView(props) {
 
     return (
         <div className='bg-[#FAFAFA] WaiterView'>
-            <header className='grid grid-flow-col fixed top-0 w-[100vw] bg-[#FAFAFA]'>
+            <header className='grid grid-flow-col grid-cols-2 fixed top-0 w-[100vw] bg-[#FAFAFA]'>
                 <img src={bqlogo} alt='Burger Queen' className='h-[13vh] p-2 ml-3 cursor-pointer' onClick={() => nav('/navigate')}/>
-                <p className='font-poppins'>Logged in as: {props.username}</p>
-                <button onClick={() => {
-                    signOut(auth).then(() => {
-                        nav('/');
-                        props.setUsername(undefined);
-                    }).catch(error => console.log(error));
-                }}
-                    className='justify-self-end self-center h-fit w-fit font-medium bg-[#1B1A1A] hover:bg-[#FE9C08] text-white shadow-md rounded-2xl px-[6%] py-[1%] mr-8'>
-                    Log out
-                </button>
+                <div className='grid grid-flow-col justify-end'>
+                    <div className='grid grid-flow-col mr-12 self-center'>
+                        <Icon color="#1B1A1A" size={26} icon="user" className='mr-2' />
+                        <p className='font-poppins self-baseline'>Logged in as: {props.username}</p>
+                    </div>
+                    <button onClick={() => {
+                        signOut(auth).then(() => {
+                            nav('/');
+                            props.setUsername(undefined);
+                        }).catch(error => console.log(error));
+                    }}
+                        className='justify-self-end self-center h-fit w-[100%] font-medium bg-[#1B1A1A] hover:bg-[#FE9C08] text-white shadow-md rounded-2xl px-[6%] py-[1%] mr-8'>
+                        Log out
+                    </button>
+                </div>
+                
             </header>
             <div className='container-chefView mt-[13vh]'>
                 <main className='main grid grid-cols-3 gap-5 m-5'>
@@ -61,7 +68,10 @@ export default function ChefView(props) {
                     )}
                 </main>
                 <aside className='aside bg-[#B5D6B2] rounded-l-2xl fixed right-0 w-[18vw] h-[80vh] overflow-auto'>
-                    <h4 className='font-poppins'>Ready</h4>
+                    <div className='flex flex-row mt-3 ml-3'>
+                        <Icon color="#1B1A1A" size={26} icon="checkmark" className='mr-2' />
+                        <h4 className='font-poppins text-xl font-medium'>Ready</h4>
+                    </div>
                     <>{completedList.map(order =>
                         <CompletedOrders
                             key={order.id}
--- src/Components/CompletedOrders/CompletedOrders.js
+++ src/Components/CompletedOrders/CompletedOrders.js
@@ -1,11 +1,13 @@
 import { getTimeDiff } from '../../ChefViewUtils';
 
 export default function CompletedOrders(props) {
-    return ( 
-        <div className="font-poppins">
-            <p>Order #{props.order.data.orderNumber}</p>
-            <p>Completed in: {getTimeDiff(props.order.data.seconds, props.order.data.completedSeconds)} min</p>
+    return (
+        <>
+            <div className="font-poppins m-2">
+                <p className='font-medium'>Order #{props.order.data.orderNumber}</p>
+                <p>Completed in: {getTimeDiff(props.order.data.seconds, props.order.data.completedSeconds)} min</p>
+            </div>
             <hr></hr>
-        </div>
+        </>
     )
 }
